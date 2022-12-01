cd1e111436b83b4cf69432aff2e5183c98846edb
Eric Liang <eric.liang@shopify.com>
2022-12-01 16:58:56 -0500

    actually day1

--- 2022/day1/solution.mjs
+++ 2022/day1/solution.mjs
@@ -1,11 +1,15 @@
 import {input} from './input.mjs';
 
 function part1(input) {
-    return input.trim().split('\n').reduce((acc, line) => 
-        line !== '' ? 
-            acc[acc.length - 1] + Number(line) :
-            acc[acc.length] = 0
-    , [0]).sort((a, b) => a - b);
+    return input.trim().split('\n').reduce((acc, line) => {
+        if (line !== '') {
+            acc[acc.length - 1] += Number(line);
+        }
+        else {
+            acc[acc.length] = 0;
+        }
+        return acc;
+    }, [0]).sort((a, b) => a - b);
 }
 
 console.dir(part1(input).pop());
