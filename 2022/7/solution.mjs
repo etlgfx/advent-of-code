d0ad37b1a8f58758107de26f2ce56346029ce67f
Eric Liang <etlgfx@gmail.com>
2022-12-21 09:10:44 -0500

    almost

--- 2022/7/solution.mjs
+++ 2022/7/solution.mjs
@@ -1,16 +1,16 @@
 import { open } from 'node:fs/promises';
 
-const file = await open('./input.txt');
+//const file = await open('./input.txt');
 
-const input = await file.readFile({ encoding: 'utf8' });
+//const input = await file.readFile({ encoding: 'utf8' });
 
-await file.close();
+//await file.close();
 
 const parse = (input) => {
     return input.trim().split('\n');
 };
 
-const solution1 = (input) => {
+const parseTree = (input) => {
     const stream = parse(input);
 
     const tree = {
@@ -24,59 +24,81 @@ const solution1 = (input) => {
 
     let mode = 'cmd';
 
-    stream.forEach(cmd => {
+    stream.forEach((cmd) => {
         let m;
 
         if (cmd.at(0) !== '$' && mode === 'ls') {
             let child = {};
 
-            if (m = cmd.match(/dir ([a-zA-Z\.]+)/)) {
+            if ((m = cmd.match(/dir ([a-zA-Z\.]+)/))) {
                 child = {
                     name: m[1],
                     size: 0,
                     children: [],
                 };
-            }
-            else if (m = cmd.match(/(\d+) ([a-zA-Z\.]+)/)) {
+            } else if ((m = cmd.match(/(\d+) ([a-zA-Z\.]+)/))) {
                 child = {
                     name: m[2],
-                    size: m[1],
+                    size: Number(m[1]),
                     children: [],
                 };
             }
 
             position.children.push(child);
-            
+
             return;
         }
 
         if (cmd.match(/\$ ls/)) {
-            console.log(`ls ${position.name}`);
             mode = 'ls';
-        }
-        else if (cmd === '$ cd ..') {
+        } else if (cmd === '$ cd ..') {
             stack.pop();
             position = stack[stack.length - 1];
-
-            console.log(position.name);
-            
-        }
-        else if (cmd === '$ cd /') {
+        } else if (cmd === '$ cd /') {
             position = tree;
             stack = [position];
-
-            console.log(position.name);
-        }
-        else if (m = cmd.match(/\$ cd ([a-zA-Z\.]+)/)) {
-            console.log(`cd to ${m[1]}`);
-            position = position.children.find(n => n.name === m[1]);
+        } else if ((m = cmd.match(/\$ cd ([a-zA-Z\.]+)/))) {
+            position = position.children.find((n) => n.name === m[1]);
             stack.push(position);
-
-            console.log(position.name);
         }
     });
 
-    console.dir(tree, {depth: 10});
+    return tree;
+};
+
+const weightTree = (tree) => {
+    if (!tree.children.length)
+        return tree.size;
+
+    tree.size = tree.children.reduce(
+        (acc, child) => acc + weightTree(child),
+        0
+    );
+
+    return tree.size;
+};
+
+const treeToList = (tree, list) => {
+
+    tree.children.forEach(node => {
+        if (node.children.length) {
+            list.concat(treeToList(tree, []));
+        }
+    });
+
+    return list;
+};
+
+const solution1 = (input) => {
+    const tree = parseTree(input);
+
+    console.dir(tree, {depth: 10})
+
+    weightTree(tree);
+
+    console.dir(tree, {depth: 10})
+
+    treeToList(tree, []);
 };
 
 const i = `
